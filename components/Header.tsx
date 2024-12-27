e318a27ec85ae93b6cec7d15f39c50aa75aa2a44
Daniel Alarcon <danny.alarcon1997@gmail.com>
2024-12-26 21:46:44 -0500

    styling header further

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
+import Form from "next/form";
 
 function Header() {
   const { user } = useUser();
@@ -12,7 +13,25 @@ function Header() {
     <header className="flex flex-wrap justify-between items-center px-4 py-2">
       {/* Top Row */}
       <div>
-        <Link href="/">FleaMarket</Link>
+        <Link
+          href="/"
+          className="
+          text=2xl 
+          font-bold 
+          text-blue-500 
+          hover:opacity-50 
+          cursor-pointer 
+          mx-auto sm:mx-0"
+        >
+          FleaMarket
+        </Link>
+
+        <Form
+          action="/search"
+          className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
+        >
+          <input type="text" name="query" />
+        </Form>
       </div>
     </header>
   );
