ec763446d006f4a287ae6450fd2bc81c81f9e9ef
Daniel Alarcon <danny.alarcon1997@gmail.com>
2024-12-27 18:48:26 -0500

    coupon code functionality

--- components/NewYearBanner.tsx
+++ components/NewYearBanner.tsx
@@ -1,5 +1,13 @@
+import { COUPON_CODES } from "@/sanity/lib/sales/couponCodes";
+import { getActiveSaleByCouponCode } from "@/sanity/lib/sales/getActiveSaleByCouponCode";
+
 async function NewYearBanner() {
-  const sale = await getActiveSaleByCouponCode("NYEAR2025");
+  const sale = await getActiveSaleByCouponCode(COUPON_CODES.NYEAR2025);
+
+  if (!sale?.isActive) {
+    return null;
+  }
+
   return <div>New Year Banner</div>;
 }
 
